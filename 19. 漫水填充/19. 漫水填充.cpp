1186c70befe061ae6188cbbc8a3e4ec4a4b8c591
陈晓聪 <a1194457876@outlook.com>
2019-07-21 21:51:15 +0800

    添加漫水填充demo以及分水岭demo

---
+++ 19. 漫水填充/19. 漫水填充.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    //读入图像
+    Mat inputImage = imread("../Image/LOL.png");
+    if (inputImage.empty())
+    {
+        exit(0);
+    }
+    imshow("输入图像", inputImage);
+
+    int lowDif = 10;
+    int upDif = 10;
+    floodFill(inputImage, Point(10, 10), Scalar(0, 0, 255), 0, Scalar(lowDif, lowDif, lowDif), Scalar(upDif, upDif, upDif));
+    imshow("效果图", inputImage);
+
+    waitKey(0);
+    return 0;
+}
---
+++ 20. 分水岭/20. 分水岭.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main()
+{
+    //读入图像
+    Mat inputImage = imread("../Image/particle.png");
+    if (inputImage.empty())
+    {
+        exit(0);
+    }
+    imshow("输入图像", inputImage);
+
+    //分水岭分割
+    Mat maskers = Mat::zeros(inputImage.size(), CV_32SC1);
+    watershed(inputImage, maskers);
+    imshow("分水岭图像", inputImage);
+
+    waitKey(0);
+    return 0;
+}
